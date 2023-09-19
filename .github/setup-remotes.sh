82ba31986cc411ac3e756f0c3f748a1cc8ab8019
fherreazcue <francisco.herreazcue@gmail.com>
2023-09-19 12:06:48 +0100

    Two shared

--- .github/setup-remotes.sh
+++ .github/setup-remotes.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-# remote_name='python-release-workflow'
-# remote_url='https://github.com/hpcflow/python-release-workflow.git'
-# dirs_to_sync='.github/'
-
 remote_name='shared1'
 remote_url='https://github.com/fherreazcue/sharedcode.git'
 dirs_to_sync='./'
 
+echo "Adding remote ${remote_url} as ${remote_name}"
 git remote add ${remote_name} ${remote_url}
+echo "Configuring as no-push"
 git remote set-url --push ${remote_name} no-pushing
+echo "Fetching ${remote_name}"
+git fetch ${remote_name}
+echo "Pulling content from ${remote_name}/main: ${dirs_to_sync}"
+git checkout ${remote_name}/main -- ${dirs_to_sync}
 
-# git fetch --all
-# git checkout ${remote_name} -- ${dirs_to_sync}
+remote_name='shared2'
+remote_url='https://github.com/fherreazcue/shared2.git'
+dirs_to_sync='d2 d3'
+
+echo "Adding remote ${remote_url} as ${remote_name}"
+git remote add ${remote_name} ${remote_url}
+echo "Configuring as no-push"
+git remote set-url --push ${remote_name} no-pushing
+echo "Fetching ${remote_name}"
+git fetch ${remote_name}
+echo "Pulling content from ${remote_name}/main: ${dirs_to_sync}"
+git checkout ${remote_name}/main -- ${dirs_to_sync}
